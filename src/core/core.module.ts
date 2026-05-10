b9b484252dd4c774d774e1bf8b0df475a236a486
Aleksandr Golodin <alex.gp.0632@yandex.ru>
2026-05-10 17:25:30 +0200

    feat: init project

---
+++ src/core/core.module.ts
@@ -0,0 +1,28 @@
+import { ApolloDriver } from '@nestjs/apollo'
+import { Module } from '@nestjs/common'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { GraphQLModule } from '@nestjs/graphql'
+
+// import { IS_DEV_ENV } from '@/shared/utils/is-dev.utils'
+
+import { getGraphQLConfig } from './config/graphql.config'
+import { PrismaModule } from './prisma/prisma.module'
+import { RedisModule } from './redis/redis.module';
+
+@Module({
+	imports: [
+		ConfigModule.forRoot({
+			isGlobal: true
+			// envFilePath: !IS_DEV_ENV
+		}),
+		GraphQLModule.forRootAsync({
+			driver: ApolloDriver,
+			// imports: [ConfigService],
+			useFactory: getGraphQLConfig,
+			inject: [ConfigService]
+		}),
+		PrismaModule,
+		RedisModule
+	]
+})
+export class CoreModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+// import { ConfigService } from '@nestjs/config'
+import { NestFactory } from '@nestjs/core'
+
+import { CoreModule } from './core/core.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+	const app = await NestFactory.create(CoreModule)
+
+	// const config
+	await app.listen(process.env.PORT ?? 3000)
 }
-bootstrap();
+
+bootstrap()
